6ecbb8e544f7e423b4ca8a151346da1f67d8b01a
Aditya Atluri <Aditya.Atluri@amd.com>
2017-08-03 17:27:20 -0500

    more debugging

--- src/gemm128x128.cpp
+++ src/gemm128x128.cpp
@@ -12,6 +12,88 @@
 #define C_X B_X
 #define C_Y A_X
 
+__device__ inline void outerProd(float4 a0, float4 a1, float4 b0, float4 b1, float4 c[]) {
+    c[0].x += a0.x * b0.x;
+    c[0].y += a0.x * b0.y;
+    c[0].z += a0.x * b0.z;
+    c[0].w += a0.x * b0.w;
+
+    c[1].x += a1.x * b0.x;
+    c[1].y += a1.x * b0.y;
+    c[1].z += a1.x * b0.z;
+    c[1].w += a1.x * b0.w;
+
+    c[2].x += a0.y * b0.x;
+    c[2].y += a0.y * b0.y;
+    c[2].z += a0.y * b0.z;
+    c[2].w += a0.y * b0.w;
+
+    c[3].x += a1.y * b0.x;
+    c[3].y += a1.y * b0.y;
+    c[3].z += a1.y * b0.z;
+    c[3].w += a1.y * b0.w;
+
+    c[4].x += a0.z * b0.x;
+    c[4].y += a0.z * b0.y;
+    c[4].z += a0.z * b0.z;
+    c[4].w += a0.z * b0.w;
+
+    c[5].x += a1.z * b0.x;
+    c[5].y += a1.z * b0.y;
+    c[5].z += a1.z * b0.z;
+    c[5].w += a1.z * b0.w;
+
+    c[6].x += a0.w * b0.x;
+    c[6].y += a0.w * b0.y;
+    c[6].z += a0.w * b0.z;
+    c[6].w += a0.w * b0.w;
+
+    c[7].x += a1.w * b0.x;
+    c[7].y += a1.w * b0.y;
+    c[7].z += a1.w * b0.z;
+    c[7].w += a1.w * b0.w;
+
+    c[8].x += a0.x * b1.x;
+    c[8].y += a0.x * b1.y;
+    c[8].z += a0.x * b1.z;
+    c[8].w += a0.x * b1.w;
+
+    c[9].x += a1.x * b1.x;
+    c[9].y += a1.x * b1.y;
+    c[9].z += a1.x * b1.z;
+    c[9].w += a1.x * b1.w;
+
+    c[10].x += a0.y * b1.x;
+    c[10].y += a0.y * b1.y;
+    c[10].z += a0.y * b1.z;
+    c[10].w += a0.y * b1.w;
+
+    c[11].x += a1.y * b1.x;
+    c[11].y += a1.y * b1.y;
+    c[11].z += a1.y * b1.z;
+    c[11].w += a1.y * b1.w;
+
+    c[12].x += a0.z * b1.x;
+    c[12].y += a0.z * b1.y;
+    c[12].z += a0.z * b1.z;
+    c[12].w += a0.z * b1.w;
+
+    c[13].x += a1.z * b1.x;
+    c[13].y += a1.z * b1.y;
+    c[13].z += a1.z * b1.z;
+    c[13].w += a1.z * b1.w;
+
+    c[14].x += a0.w * b1.x;
+    c[14].y += a0.w * b1.y;
+    c[14].z += a0.w * b1.z;
+    c[14].w += a0.w * b1.w;
+
+    c[15].x += a1.w * b1.x;
+    c[15].y += a1.w * b1.y;
+    c[15].z += a1.w * b1.z;
+    c[15].w += a1.w * b1.w;
+
+}
 
 __global__ void Gemm128x128(float4 *A, float4 *B, float4 *C) {
     int tx = hipThreadIdx_x;
@@ -22,6 +104,7 @@ __global__ void Gemm128x128(float4 *A, float4 *B, float4 *C) {
     int gmLoadBtx = tx;
 
     int sAtx = (tx%2) * 128 * 4 + tx/2;
+    int ldsStoreAtx = tx;
     int ldsStoreBtx = tx;
 
     float *sA = (float*)sAx4;
@@ -34,99 +117,25 @@ __global__ void Gemm128x128(float4 *A, float4 *B, float4 *C) {
 
     sBx4[ldsStoreBtx] = B[gmLoadBtx];
 
-    float4 a0, a1, b0, b1, c0, c1;//[8][2];
+
+    float4 a0, a1, b0, b1, c[8*2];
+
+    int gmStoreCtx = (tx%16)*2 + (tx/16)*128*8;
+    for(int i=0;i<8*2;i++) {
+        c[i] = 1.0f;
+    }
+
     a0 = sAx4[tx%16];
     a1 = sAx4[tx%16+16];
 
     b0 = sBx4[tx%16];
     b1 = sBx4[tx%16+16];
+
+    outerProd(a0, a1, b0, b1, c);
+
 /*
-    c[0][0].x = a0.x * b0.x;
-    c[0][0].y = a0.x * b0.y;
-    c[0][0].z = a0.x * b0.z;
-    c[0][0].w = a0.x * b0.w;
-
-    c[0][1].x = a1.x * b0.x;
-    c[0][1].y = a1.x * b0.y;
-    c[0][1].z = a1.x * b0.z;
-    c[0][1].w = a1.x * b0.w;
-
-    c[1][0].x = a0.y * b0.x;
-    c[1][0].y = a0.y * b0.y;
-    c[1][0].z = a0.y * b0.z;
-    c[1][0].w = a0.y * b0.w;
-
-    c[1][1].x = a1.y * b0.x;
-    c[1][1].y = a1.y * b0.y;
-    c[1][1].z = a1.y * b0.z;
-    c[1][1].w = a1.y * b0.w;
-
-    c[2][0].x = a0.z * b0.x;
-    c[2][0].y = a0.z * b0.y;
-    c[2][0].z = a0.z * b0.z;
-    c[2][0].w = a0.z * b0.w;
-
-    c[2][1].x = a1.z * b0.x;
-    c[2][1].y = a1.z * b0.y;
-    c[2][1].z = a1.z * b0.z;
-    c[2][1].w = a1.z * b0.w;
-
-    c[3][0].x = a0.w * b0.x;
-    c[3][0].y = a0.w * b0.y;
-    c[3][0].z = a0.w * b0.z;
-    c[3][0].w = a0.w * b0.w;
-
-    c[3][1].x = a1.w * b0.x;
-    c[3][1].y = a1.w * b0.y;
-    c[3][1].z = a1.w * b0.z;
-    c[3][1].w = a1.w * b0.w;
-
-    c[4][0].x = a0.x * b1.x;
-    c[4][0].y = a0.x * b1.y;
-    c[4][0].z = a0.x * b1.z;
-    c[4][0].w = a0.x * b1.w;
-
-    c[4][1].x = a1.x * b1.x;
-    c[4][1].y = a1.x * b1.y;
-    c[4][1].z = a1.x * b1.z;
-    c[4][1].w = a1.x * b1.w;
-
-    c[5][0].x = a0.y * b1.x;
-    c[5][0].y = a0.y * b1.y;
-    c[5][0].z = a0.y * b1.z;
-    c[5][0].w = a0.y * b1.w;
-
-    c[5][1].x = a1.y * b1.x;
-    c[5][1].y = a1.y * b1.y;
-    c[5][1].z = a1.y * b1.z;
-    c[5][1].w = a1.y * b1.w;
-
-    c[6][0].x = a0.z * b1.x;
-    c[6][0].y = a0.z * b1.y;
-    c[6][0].z = a0.z * b1.z;
-    c[6][0].w = a0.z * b1.w;
-
-    c[6][1].x = a1.z * b1.x;
-    c[6][1].y = a1.z * b1.y;
-    c[6][1].z = a1.z * b1.z;
-    c[6][1].w = a1.z * b1.w;
-
-    c[7][0].x = a0.w * b1.x;
-    c[7][0].y = a0.w * b1.y;
-    c[7][0].z = a0.w * b1.z;
-    c[7][0].w = a0.w * b1.w;
-
-    c[7][1].x = a1.w * b1.x;
-    c[7][1].y = a1.w * b1.y;
-    c[7][1].z = a1.w * b1.z;
-    c[7][1].w = a1.w * b1.w;
-*/
-    c0 = a0 + b0;
-    c1 = a1 + b1;
-
-    int gmStoreCtx = (tx%16)*2 + (tx/16)*8*128;
     C[gmStoreCtx + 0*32 + 0] = c0;
-/*    C[gmStoreCtx + 0*32 + 1] = c1;
+    C[gmStoreCtx + 0*32 + 1] = c1;
     C[gmStoreCtx + 1*32 + 0] = c0;
     C[gmStoreCtx + 1*32 + 1] = c1;
     C[gmStoreCtx + 2*32 + 0] = c0;
@@ -142,6 +151,8 @@ __global__ void Gemm128x128(float4 *A, float4 *B, float4 *C) {
     C[gmStoreCtx + 7*32 + 0] = c0;
     C[gmStoreCtx + 7*32 + 1] = c1;
 */
+
+    C[gmStoreCtx] = c[0];
 }
 
 int main(){
@@ -149,8 +160,8 @@ int main(){
     std::vector<float> B(B_X*B_Y);
     std::vector<float> C(C_X*C_Y);
 
-    std::fill(A.begin(), A.end(), 1.0f);
-    std::fill(B.begin(), B.end(), 1.0f);
+    std::fill(A.begin(), A.end(), 2.0f);
+    std::fill(B.begin(), B.end(), 2.0f);
     std::fill(C.begin(), C.end(), 0.0f);
 
     float *Ad, *Bd, *Cd;
--- src/tcal.py
+++ src/tcal.py
@@ -4,8 +4,6 @@ Calculate different thread patterns
 
 tx = list(range(0, 256))
 
-print("sAtx", map(lambda x: (x%2) * 512 + x/2, tx))
-print("gmStoreCtx", map(lambda x: (x%16)*2 + (x/16)*8*128 + 7*32 + 1, tx))
-print("a0", map(lambda x: x%16, tx))
-print("a1", map(lambda x: x%16+16, tx))
+#print("sAtx", map(lambda x: (x%2) * 512 + x/2, tx))
+print("gmStoreCtx", map(lambda x: (x%16)*2 + (x/16)*128*8, tx))
 print(128*128)
